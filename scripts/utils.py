6985267ce17258a3920b2b2ad5721878e70befe8
Josh Hadley <johadley@adobe.com>
2022-10-20 14:41:24 -0700

    Remove unused file & code

--- scripts/utils.py
+++ scripts/utils.py
@@ -20,16 +20,3 @@ groups = [
     ]
 
 styles = ["3D", "Color", "Flat", "High Contrast"]
-
-
-def get_popular(max_rank: int):
-    # parse the ranked list file and return a set with the first 'max_rank' names
-    popular = set()
-    rankfilepath = Path("data") / "2021_ranked.tsv"
-    with open(rankfilepath, 'r') as rf:
-        reader = csv.DictReader(rf, delimiter='\t')
-        for row in reader:
-            if int(row['Rank']) <= max_rank:
-                popular.add(row['Name'])
-
-    return popular
